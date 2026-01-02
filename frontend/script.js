c6dbb4a6812dc4132f0857ca97e10dac3fb37f7b
Nathan Gu <nathan.gu12345@gmail.com>
2026-01-01 16:17:50 -0800

    Remove everything related to vercel

--- frontend/script.js
+++ frontend/script.js
@@ -77,7 +77,7 @@ async function sendMessage() {
   showTypingIndicator();
 
   try {
-    const response = await fetch("/api/chat", {
+    const response = await fetch("http://127.0.0.1:8000/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
